b64b1d9aafb53961535bc1c108b88f07be16689c
小孙 <731238433@qq.com>
2021-12-06 21:39:23 +0800

    格式化登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,7 +9,9 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">
+          Login Form
+        </h3>
       </div>
 
       <el-form-item prop="username">
@@ -42,13 +44,23 @@
           auto-complete="on"
           @keyup.enter="handleLogin"
         />
-        <span class="show-pwd" @click="showPwd">
+        <span
+          class="show-pwd"
+          @click="showPwd"
+        >
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" size="medium" style="width:100%;margin-bottom:30px;" @click.prevent="handleLogin">Login</el-button>
-
+      <el-button
+        :loading="loading"
+        type="primary"
+        size="medium"
+        style="width:100%;margin-bottom:30px;"
+        @click.prevent="handleLogin"
+      >
+        Login
+      </el-button>
     </el-form>
   </div>
 </template>
